19ff2cb3b11b75d0c843ac87fcee512d338d1ead
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-02-26 15:49:32 +0530

    Added nodemon and dotenv. Configured backend accordingly

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`)
