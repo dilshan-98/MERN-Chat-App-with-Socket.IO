b159087a522afaa278df4bc14ee1842c2225f569
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-02-26 16:01:05 +0530

    Added routes to display static data

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import chats from "./data/data.js";
 
 dotenv.config();
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is running!");
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const id = req.params.id;
+    const chat = chats.find(chat => chat._id === id);
+
+    res.send(chat);
+});
 
 const PORT = process.env.PORT;
 
