05bb5f45e5984c92c819cf69869b509657343ca8
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-03-11 00:36:46 +0530

    init reg

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,11 +1,102 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Register = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );   
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [pic, setPic] = useState();
+  const [show, setShow] = useState(false);
+
+  const toggleClick = () => setShow(!show);
+
+  const postDetails = (pic) => {};
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="full-name" isRequired>
+        <FormLabel>
+          Full Name
+        </FormLabel>
+        <Input
+          placeholder="Enter your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>
+          Email
+        </FormLabel>
+        <Input
+          placeholder="Enter your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>
+          Password
+        </FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={toggleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirm-password" isRequired>
+        <FormLabel>
+          Confirm Password
+        </FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm your Password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={toggleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic" isRequired>
+        <FormLabel>Upload you Avatar</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100"
+        style={{ marginTop: 15 }}
+        color="white"
+        onClick={submitHandler}
+      >
+        Submit
+      </Button>
+    </VStack>
+  );
 };
 
 export default Register;
