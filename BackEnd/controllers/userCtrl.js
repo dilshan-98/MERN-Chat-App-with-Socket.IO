770bd30db8fa306c6e40dc55302f624aefc4c380
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-03-12 20:49:14 +0530

    user login

--- BackEnd/controllers/userCtrl.js
+++ BackEnd/controllers/userCtrl.js
@@ -4,35 +4,50 @@ import user from "../Models/userModel.js";
 const userRegister = async (req, res) => {
   const { name, email, password, pic } = req.body;
 
-  if(!name || !email || !password) {
-    return res.status(404).json({msg: "Missing credentials"});
+  if (!name || !email || !password) {
+    return res.status(404).json({ msg: "Missing credentials" });
     // throw new Error("Missing credentials");
   }
 
   const userExists = await user.findOne({ email });
 
   if (userExists) {
-    return res.status(400).json({msg: "User already Exists"});
+    return res.status(400).json({ msg: "User already Exists" });
     // throw new Error("User already Exists");
   }
 
-  const userSaved = await user.create({name, email, password, pic});
+  const userSaved = await user.create({ name, email, password, pic });
 
   if (userSaved) {
-    return res
-      .status(201)
-      .json({
-        msg: "Successfully registered",
-        token: generateToken(userSaved._id),
-      });
-  }else{
-    return res.status(400).json({msg: "User Registration Unsuccessful"});
+    return res.status(201).json({
+      msg: "Successfully registered",
+      token: generateToken(userSaved._id),
+    });
+  } else {
+    return res.status(400).json({ msg: "User Registration Unsuccessful" });
     // throw new Error("User Registration Unsuccessful");
   }
 };
 
 const login = async (req, res) => {
-  return res.status(200).json({ msg: "Login successful" });
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(404).json({ msg: "Missing credentials" });
+
+  const userExists = await user.findOne({ email });
+
+  if (!userExists) {
+    return res.status(400).json({ msg: "Invalid credentials" });
+  }
+
+  const currentUser = await user.findOne({ email, password });
+
+  if (currentUser) {
+    return res.status(200).json({ msg: "Login successful", token: generateToken(currentUser._id) });
+  } else {
+    return res.status(400).json({ msg: "Invalid credentials" });
+  }
 };
 
 export { userRegister, login };
